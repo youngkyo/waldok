a15e6a63fc50cd9118c2822d6bd61792ec445726
ykyo.jung <ykyo.jung@coupang.com>
2018-08-26 17:03:58 +0900

    [wal-001] project start

--- source/main.py
+++ source/main.py
@@ -1,22 +1,21 @@
 import pandas as pd
 import telegram
-my_token = '643538405:AAGm9ZRYHtSEuLwJMazk7_bTnjlvC9Jx-4w'
+import configparser
 
-# 토큰을 지정해서 bot을 선언해 줍시다! (물론 이 토큰은 dummy!)
+config = configparser.ConfigParser()
+config.read('/home/ec2-user/stock/config/waldokconfig')
+
+my_token = config['DEFAULT']['TELE_TOKEN']
 bot = telegram.Bot(token=my_token)
 
 
 def all_stock_name_by_df():
     code_df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13', header=0)[0]
-    #  종목코드가 6자리이기 때문에 6자리를 맞춰주기 위해 설정해줌
     code_df.종목코드 = code_df.종목코드.map('{:06d}'.format)
-    #  우리가 필요한 것은 회사명과 종목코드이기 때문에 필요없는 column들은 제외해준다.
+
     code_df = code_df[['회사명', '종목코드']]
-    #  한글로된 컬럼명을 영어로 바꿔준다.
 
     code_df = code_df.rename(columns={'회사명': 'name', '종목코드': 'code'})
-    # for index, row in code_df.iterrows():
-    #     print(row['code'], row['name'])
     return code_df
 
 
@@ -29,16 +28,50 @@ def get_url(item_name, code_df):
     return url
 
 
+def is_desire_volume(volume):
+    if volume > 1000000:
+        return True
+
+    return False
+
+
+def is_desire_volume_reduction(volumn):
+    if volumn > 11:
+        return True
+
+    return False
+
+
+def is_bull_day(start_price, end_price):
+    if start_price < end_price:
+        return True
+
+    return False
+
+
+def is_bear_day(start_price, end_price):
+    if start_price > end_price:
+        return True
+
+    return False
+
+
 def check_data_frame(df):
     volume_list = list()
+    start_price_list = list()
+    end_price_list = list()
+
     try:
         for index in range(1, 6):
             volume_list.append(df.iloc[index]['거래량'])
+            start_price_list.append(df.iloc[index]['시가'])
+            end_price_list.append(df.iloc[index]['종가'])
 
-        a = volume_list[4]
-        for index in range(1, 4):
-            value = volume_list[index] / a
-            if volume_list[index] > 1000000 and value > 11:
+        today_volume = df.iloc[0]['거래량']
+        for index in range(1, 6):
+            value = volume_list[index] / today_volume
+            if is_desire_volume(volume_list[index]) and is_desire_volume_reduction(value) and is_bull_day(
+                    start_price_list[index], end_price_list[index]) and is_bull_day(df.iloc[0]['시가'], df.iloc[0]['종가']):
                 return True
 
     except IndexError:
@@ -69,7 +102,7 @@ def main():
     value = get_head_subject(stock_df)
     value_list = '\n'.join(value)
 
-    # bot.send_message(chat_id='@waldok', text=value_list, timeout=10)
+    bot.send_message(chat_id='@waldok', text=value_list, timeout=10)
 
 
 if __name__ == "__main__":
