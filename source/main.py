86a3d0769a9cd654904d01b866888624f9e1b3bf
ykyo.jung <ykyo.jung@coupang.com>
2018-09-02 13:07:07 +0900

    [wal-001] project start

--- source/main.py
+++ source/main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import telegram
 import configparser
+from bs4 import BeautifulSoup
+import requests
 
 config = configparser.ConfigParser()
 config.read('/home/ec2-user/stock/config/waldokconfig')
@@ -9,14 +11,43 @@ my_token = config['DEFAULT']['TELE_TOKEN']
 bot = telegram.Bot(token=my_token)
 
 
+def getStockList(kospi_url):
+    stock_list = []
+
+    for i in range(1, 2000):
+        url = kospi_url + str(i)
+        req = requests.get(url)
+        soup = BeautifulSoup(req.text, 'lxml')
+        items = soup.find_all('tr', {'onmouseover':'mouseOver(this)'})
+        if items.__len__() is 0:
+            break
+
+        for index in range(0, len(items)):
+            # items[0].find_all('tr', class_="number")
+            # print(items[index].a.get('href')[-6:]) #종목코드
+            # print(items[index].a.text) # 종목명
+            # print(items[index].find_all("td", class_="number")[4].text) #시가총액
+            # print(items[index].find_all("td", class_="number")[7].text) #거래량
+            items_list = items[index]
+            class_number_list = items_list.find_all("td", class_="number")
+            asset = str(class_number_list[4].text).replace(",", "")
+            volume = str(class_number_list[7].text).replace(",", "")
+
+            if int(volume) > 100000 and int(asset) > 400:
+                stock_list.append([items_list.a.get('href')[-6:], items_list.a.text, class_number_list[4].text])
+
+    return stock_list
+
+
 def all_stock_name_by_df():
-    code_df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13', header=0)[0]
-    code_df.종목코드 = code_df.종목코드.map('{:06d}'.format)
 
-    code_df = code_df[['회사명', '종목코드']]
+    kospi_url = 'https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0&&page='
+    kosdaq_url = 'https://finance.naver.com/sise/sise_market_sum.nhn?sosok=1&page='
 
-    code_df = code_df.rename(columns={'회사명': 'name', '종목코드': 'code'})
-    return code_df
+    final_list = getStockList(kospi_url)
+    final_list.extend(getStockList(kosdaq_url))
+
+    return pd.DataFrame(final_list, columns=['code', 'name', 'volume'])
 
 
 # 종목 이름을 입력하면 종목에 해당하는 코드를 불러와 # 네이버 금융(http://finance.naver.com)에 넣어줌
@@ -24,7 +55,6 @@ def get_url(item_name, code_df):
     code = code_df.query("name=='{}'".format(item_name))['code'].to_string(index=False)
     url = 'http://finance.naver.com/item/sise_day.nhn?code={code}'.format(code=code)
 
-    # print("요청 URL = {}".format(url))
     return url
 
 
@@ -70,8 +100,7 @@ def check_data_frame(df):
         today_volume = df.iloc[0]['거래량']
         for index in range(1, 6):
             value = volume_list[index] / today_volume
-            if is_desire_volume(volume_list[index]) and is_desire_volume_reduction(value) and is_bull_day(
-                    start_price_list[index], end_price_list[index]) and is_bull_day(df.iloc[0]['시가'], df.iloc[0]['종가']):
+            if is_desire_volume(volume_list[index]) and is_desire_volume_reduction(value) and is_bull_day(start_price_list[index], end_price_list[index]):
                 return True
 
     except IndexError:
@@ -101,8 +130,8 @@ def main():
     stock_df = all_stock_name_by_df()
     value = get_head_subject(stock_df)
     value_list = '\n'.join(value)
-
-    bot.send_message(chat_id='@waldok', text=value_list, timeout=10)
+    if value_list != "":
+        bot.send_message(chat_id='@waldok', text=value_list, timeout=10)
 
 
 if __name__ == "__main__":
